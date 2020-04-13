d705925b7ae6a69dad773f7e00a6d915b7a514c7
Michael J Fox <michael@nicepowered.com>
2020-04-13 10:46:58 -0700

    sort videos by published date

--- src/views/Gallery/index.js
+++ src/views/Gallery/index.js
@@ -2,6 +2,7 @@ import React, {
   useContext,
   useState,
 } from 'react';
+import { DateTime } from 'luxon';
 
 import { VideoContext } from '../../data/providers/providers/videos';
 import VideoView from './VideoView';
@@ -38,24 +39,37 @@ const Component = () => {
 
   let colorIdx = 0;
 
-  const videoViews = videos.map((v) => {
-    const color = COLOR_OPTIONS[colorIdx];
-    if (colorIdx === 3) {
-      colorIdx = 0;
-    } else {
-      colorIdx += 1;
-    }
+  const videoViews = videos
+    .sort((a, b) => {
+      const aTS = DateTime.fromISO(a.publishedAt).toMillis();
+      const bTS = DateTime.fromISO(b.publishedAt).toMillis();
 
-    return (
-      <VideoView
-        key={`video-${v.id}`}
-        video={v}
-        color={color}
-        previous={previous}
-        next={next}
-      />
-    );
-  });
+      if (aTS > bTS) {
+        return -1;
+      } else if (bTS > aTS){
+        return 1;
+      } else {
+        return 0;
+      }
+    })
+    .map((v) => {
+      const color = COLOR_OPTIONS[colorIdx];
+      if (colorIdx === 3) {
+        colorIdx = 0;
+      } else {
+        colorIdx += 1;
+      }
+
+      return (
+        <VideoView
+          key={`video-${v.id}`}
+          video={v}
+          color={color}
+          previous={previous}
+          next={next}
+        />
+      );
+    });
 
   return (
     <div className={styles.view}>
