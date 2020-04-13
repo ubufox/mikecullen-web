fe4771e7009d6158fc56b1c9197d035f91f00a9e
Michael J Fox <michael@nicepowered.com>
2020-04-13 00:43:33 -0700

    dont rerender iframe on orientation change

--- src/views/VideoFrame/IFrameByScreen.js
+++ src/views/VideoFrame/IFrameByScreen.js
@@ -1,67 +1,23 @@
 import React from 'react';
-import MediaQuery from 'react-responsive';
 
-import {
-  MOBILE_WIDTH,
-  XL_MOBILE_WIDTH,
-} from '../../styles/media_queries';
-
+import styles from './iframe.module.scss';
 
 const Component = ({
   title,
   url,
 }) => {
   return (
-    <>
-      <MediaQuery maxWidth={374}>
-        <iframe
-          title={title}
-          width="320"
-          height="180"
-          src={`https://${url}?autoplay=1&rel=0`}
-          frameBorder="0"
-          allowFullScreen={true}
-          allow="autoplay; encrypted-media;"
-        >
-        </iframe>
-      </MediaQuery>
-      <MediaQuery minWidth={375} maxWidth={MOBILE_WIDTH - 1}>
-        <iframe
-          title={title}
-          width="375"
-          height="210"
-          src={`https://${url}?autoplay=1&rel=0`}
-          frameBorder="0"
-          allowFullScreen={true}
-          allow="autoplay; encrypted-media;"
-        >
-        </iframe>
-      </MediaQuery>
-      <MediaQuery maxWidth={XL_MOBILE_WIDTH - 1} minWidth={MOBILE_WIDTH}>
-        <iframe
-          title={title}
-          width="414"
-          height="232"
-          src={`https://${url}?autoplay=1&rel=0`}
-          frameBorder="0"
-          allowFullScreen={true}
-          allow="autoplay; encrypted-media;"
-        >
-        </iframe>
-      </MediaQuery>
-      <MediaQuery minWidth={XL_MOBILE_WIDTH}>
-        <iframe
-          title={title}
-          width="600"
-          height="337"
-          src={`https://${url}?autoplay=1&rel=0`}
-          frameBorder="0"
-          allowFullScreen={true}
-          allow="autoplay; encrypted-media;"
-        >
-        </iframe>
-      </MediaQuery>
-    </>
+    <div className={styles.iframeContainer}>
+      <iframe
+        className={styles.iframeElement}
+        title={title}
+        src={`https://${url}?autoplay=1&rel=0`}
+        frameBorder="0"
+        allowFullScreen={true}
+        allow="autoplay; encrypted-media;"
+      >
+      </iframe>
+    </div>
   );
 };
 
